9a853509c579e323a63a1d41aa909f75cf37e0ad
Jessica <jessicaliang26@gmail.com>
2023-04-19 21:17:35 -0700

    set up basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import './stylesheets/App.css';
+import {BrowserRouter as Router, Routes, Route, BrowserRouter} from "react-router-dom"
+import {Switch} from 'react-router'
+import Login from "./component/login.js"
+import CreateAcc from "./component/createAcc.js"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-      
-    </div>
+  
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="/create-account" element={<CreateAcc />}/>
+      </Routes>
+    </Router>
+    
   );
 }
 
---
+++ frontend/src/component/createAcc.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CreateAcc = () => {
+    return(
+      <h1>Create Account Page placeholder</h1>
+    )
+  }
+  
+  export default CreateAcc
